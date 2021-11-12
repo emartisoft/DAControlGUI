7e240e5581397f2582a7dc7bec59987ead4cac02
emarti <dtemarti@gmail.com>
2021-11-12 09:11:30 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,8 +1,13 @@
 #ifndef VERSION_H
 #define VERSION_H
 // version
-#define MAJOR   "1.0"
-#define MINOR   "1110"
-#define ABOUT "DAControlGUI "MAJOR"."MINOR" (C) 2021\nCoded by emarti, Murat OZDEMIR\n\nWebSite:\nhttps://github.com/emartisoft/DAControlGUI"
-UBYTE *vers = "$VER: DAControlGUI "MAJOR" (29.10.2021) emarti, Murat Ozdemir";
+#define MAJOR   "1"
+#define MINOR   "0"
+#define BUILD   "1110"
+#define DATE    "29.10.2021"
+#define CODER   "emarti, Murat OZDEMIR"
+#define APPNAME "DAControlGUI"
+
+#define ABOUT APPNAME" "MAJOR"."MINOR"."BUILD" (C) 2021\nCoded by "CODER"\n\nWebSite:\nhttps://github.com/emartisoft/"APPNAME
+UBYTE *vers = "$VER: "APPNAME" "MAJOR"."MINOR" ("DATE") "CODER;
 #endif
