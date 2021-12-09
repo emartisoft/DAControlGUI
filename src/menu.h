0d65a27043500088a4e20da53aed63c0dc3d5e8b
Chris Young <chris@unsatisfactorysoftware.co.uk>
2021-12-09 16:29:35 +0000

    Style guide Menu items opening requesters should end with ellipsis

--- src/menu.h
+++ src/menu.h
@@ -24,15 +24,15 @@
 struct NewMenu dacMenuNewMenu[] =
 {
     NM_TITLE, (STRPTR)"DAControlGUI"               	,  NULL , 0, 0L, (APTR)~0,
-    NM_ITEM , (STRPTR)"About"               		,  NULL , 0, 0L, (APTR)~0,
+    NM_ITEM , (STRPTR)"About..."               		,  NULL , 0, 0L, (APTR)~0,
     NM_ITEM , NM_BARLABEL                          	,  NULL , 0, 0L, (APTR)~0,
     NM_ITEM , (STRPTR)"Iconify"                     ,  "I" , 0, 0L, (APTR)~0,
     NM_ITEM , NM_BARLABEL                          	,  NULL , 0, 0L, (APTR)~0,
     NM_ITEM , (STRPTR)"Quit"                      	,  "Q" , 0, 0L, (APTR)~0,
 	
     NM_TITLE, (STRPTR)"Controls"              		,  NULL , 0, 0L, (APTR)~0,
-    NM_ITEM , (STRPTR)"Load / Change ADF" 			,  "L" , 0, 0L, (APTR)~0,
-    NM_ITEM , (STRPTR)"Create ADF"                  ,  "C" , 0, 0L, (APTR)~0,
+    NM_ITEM , (STRPTR)"Load / Change ADF..." 			,  "L" , 0, 0L, (APTR)~0,
+    NM_ITEM , (STRPTR)"Create ADF..."                  ,  "C" , 0, 0L, (APTR)~0,
 	NM_ITEM , NM_BARLABEL                          	,  NULL , 0, 0L, (APTR)~0,
 	NM_ITEM , (STRPTR)"Eject ADF"                 	,  "E" , 0, 0L, (APTR)~0,
     NM_ITEM , (STRPTR)"Eject All"            		,  "A" , 0, 0L, (APTR)~0,
@@ -40,7 +40,7 @@ struct NewMenu dacMenuNewMenu[] =
 	NM_ITEM , (STRPTR)"Refresh"                 	,  "R" , 0, 0L, (APTR)~0,
     
     NM_TITLE, (STRPTR)"Help"                       ,  NULL , 0, 0L, (APTR)~0,
-    NM_ITEM , (STRPTR)"Manual"                      ,  "M" , 0, 0L, (APTR)~0,
+    NM_ITEM , (STRPTR)"Manual..."                      ,  "M" , 0, 0L, (APTR)~0,
 	
     NM_END  , NULL                                 ,  NULL , 0, 0L, (APTR)~0
 };
